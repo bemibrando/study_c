2c45bfe18299fb4acaee0d72a565e3c8b531f4d8
bemibrando <bemi.brando@outlook.com>
2025-02-12 22:44:11 -0300

    C Descomplicada - Hello World
    Project to understand the skeleton of a C program

---
+++ c_descomplicada/001_hello_world/hello_world/hello_world.cpp
@@ -0,0 +1,16 @@
+// Standard inpud and output library
+#include <stdio.h>
+// Standard library for memory management, system funcions, etc.
+#include <stdlib.h>
+
+// Main function: Entry point of the program
+int main() {
+	// Prints "Hello World!" to the console
+	printf("Hello World!\n");
+
+	// Keeps the console window open until the user presses a key (Windows only)
+	system("pause");
+
+	// Returns 0, indicating that the program executed successfully
+	return 0;
+}
