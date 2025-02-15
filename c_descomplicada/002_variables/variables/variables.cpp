fd295f9fbd0294759e3869cc6c72d58baf036860
bemibrando <bemi.brando@outlook.com>
2025-02-14 22:22:14 -0300

    C Descomplicada - Variables
    Code developed to learn how to declare variables.

---
+++ c_descomplicada/002_variables/variables/variables.cpp
@@ -0,0 +1,28 @@
+// C++ standard library for input and output operations
+#include <iostream>
+
+int main()
+{
+//  Variable declarations:
+// 	   type variableName = value;
+// 	Variable Naming Rules.
+//    - Must start with a letter (A-Z or a-z) or an underscore(_)
+//	  - Cannot start with a number
+//    - Can only contain letters, numbers, and underscores
+//    - Cannot be a reserved keyword (e.g., int, return, if, etc.)
+//    - Case-sensitive ('Age' and 'age' are different variables)
+ 
+	// Stores a single character
+	char letra = 'C';
+
+	//  Stores integer values
+	int numero1 = 13;
+
+	// Stores real (floating-point) numbers with single precision
+	float pi = 3.1415;
+
+	// Stores real numbers with high precision (double precision floating-point)
+	double mol = 6.0e23;
+
+	return 0;
+}
